05b81a1e14d5699994f56a9655d80d3fa8530968
jakub_02 <baransky@spse-po.sk>
2022-04-24 08:57:26 +0200

    fixes

--- app/src/main/java/com/example/tatrakids/HomeActivity.java
+++ app/src/main/java/com/example/tatrakids/HomeActivity.java
@@ -2,16 +2,19 @@ package com.example.tatrakids;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+    CardView balance, education, map, saving, transactions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +22,27 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_home);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        balance = findViewById(R.id.balance);
+        education = findViewById(R.id.education);
+        map = findViewById(R.id.map);
+        saving = findViewById(R.id.saving);
+        transactions = findViewById(R.id.transactions);
+
+        balance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, BalanceActivity.class));
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        }
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        switch (id){
+        switch (id) {
             case R.id.home:
                 break;
 
